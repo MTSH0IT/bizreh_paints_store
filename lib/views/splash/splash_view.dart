adc2af3c3d0dc9cdc10a30095f31d6b1293bfd29
MTSH0IT <malshurbaji.11@gmail.com>
2026-05-30 13:29:15 +0300

    feat: add splash screen with automatic authentication check and include assets directory

--- lib/views/splash/splash_view.dart
+++ lib/views/splash/splash_view.dart
@@ -1,6 +1,7 @@
 import 'package:bizreh_paints_store/controllers/auth_controller.dart';
 import 'package:bizreh_paints_store/views/auth/signIn_view.dart';
 import 'package:bizreh_paints_store/views/mainView/main_view.dart';
+import 'package:bizreh_paints_store/utils/consts/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -15,13 +16,13 @@ class _SplashViewState extends State<SplashView> {
   @override
   void initState() {
     super.initState();
-
     _load();
   }
 
   Future<void> _load() async {
     final auth = Get.find<AuthController>();
     final ok = await auth.tryAutoLogin();
+
     if (!mounted) return;
     if (ok) {
       Get.offAll(() => const MainView());
@@ -32,6 +33,31 @@ class _SplashViewState extends State<SplashView> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: CircularProgressIndicator()));
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.white, backgroundColor],
+          ),
+        ),
+        child: SafeArea(
+          child: Stack(
+            children: [
+              Center(
+                child: Image.asset(
+                  'assets/images/logo.png',
+                  width: 180,
+                  height: 180,
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
